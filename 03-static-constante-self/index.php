f7c8f7f841e796ddd50132271ad0e61631678ae6
Trykxx <amitnicolas@gmail.com>
2024-07-01 16:55:17 +0200

    mathematiques

--- 03-static-constante-self/index.php
+++ 03-static-constante-self/index.php
@@ -1,7 +1,12 @@
 <?php
 
 require_once './SimulateurCredit.php';
+require_once './Mathematiques.php';
 
-echo SimulateurCredit::TAUX;
-echo '<br>';
-echo SimulateurCredit::calculInteret(10000);
+echo SimulateurCredit::TAUX . '<br>';
+echo SimulateurCredit::calculInteret(10000) . '<br>';
+
+echo Mathematiques::addition(10,2). '<br>';
+echo Mathematiques::division(10,2). '<br>';
+echo Mathematiques::multiplication(10,2). '<br>';
+echo Mathematiques::soustraction(10,2). '<br>';
